a314ad924de33b6f780b89a1d8736ece469a0238
sam50527969 <sam50527969@gmail.com>
2026-08-29 07:10:20 +0300

    Add CEO execution history console (#10)

---
+++ frontend/src/api/ceoExecutions.js
@@ -0,0 +1,42 @@
+﻿import { request } from "./client";
+
+
+export function getCEOExecutions({
+  limit = 50,
+  offset = 0,
+} = {}) {
+  const searchParams =
+    new URLSearchParams();
+
+  searchParams.set(
+    "limit",
+    String(limit),
+  );
+
+  searchParams.set(
+    "offset",
+    String(offset),
+  );
+
+  return request(
+    `/ceo-executions?${searchParams.toString()}`,
+  );
+}
+
+
+export function getCEOExecution(
+  executionUid,
+) {
+  return request(
+    `/ceo-executions/${executionUid}`,
+  );
+}
+
+
+export function getCEOExecutionByApproval(
+  approvalUid,
+) {
+  return request(
+    `/ceo-executions/approval/${approvalUid}`,
+  );
+}
